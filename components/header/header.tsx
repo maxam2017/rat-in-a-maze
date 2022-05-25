8e89cb8f1eb1860c76aef28982f7e9ed82f3c9a7
maxam2017 <whk0313@gmail.com>
2022-05-25 14:40:26 +0800

    feat: add github link

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,11 +1,26 @@
 import ThemeSwitch from "components/theme-switch";
 import styles from "./header.module.scss";
+import Github from "assets/svg/github.svg";
+import Link from "next/link";
+
+function GithubLink() {
+  return (
+    <Link href="https://github.com/maxam2017/rat-in-a-maze" passHref={true}>
+      <a title="GitHub" className={styles.header__addon__link}>
+        <Github data-tip="hello world" className={styles.header__addon__link} />
+      </a>
+    </Link>
+  );
+}
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <h1 className={styles.header__title}>🐭 in a Maze</h1>
-      <ThemeSwitch />
+      <div className={styles.header__addon}>
+        <ThemeSwitch />
+        <GithubLink />
+      </div>
     </header>
   );
 }
